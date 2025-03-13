78e675392624ba640027ac86a294802cbe136eb4
simofb21 <simone.fusarbassini@galileo.galileicrema.it>
2025-03-13 21:07:23 +0100

    sistemato
    ora legge e fa richiesta che aggiorna correttamente. quindi andrà sistemata la web app

--- leggiDa/code.py
+++ leggiDa/code.py
@@ -1,39 +1,53 @@
 import serial
 import requests
-import time
-# Configura la porta seriale
+import json
+# lo usiamo da windows
+
 ser = serial.Serial(
-    #port='/dev/serial0', 
-    port='/dev/ttyUSB0',
+    port='COM5',  #così è da pc , sarà da cambiare con la porta seriale del raspberry
     baudrate=9600,        
     timeout=1            
 )
-url = "http://10.0.1.13:5000/update_position" # qua sistemerò dopo 
+
+url = "http://192.168.1.12:5000/update_position"  # da cambiare con ip raspberry a scuola
+
+print("Listening for messages from ARDUINO...")
 
 def send_position(x, y):
     """
-    Invia la posizione (x, y) al server Flask.
+    Invio la posizione (x, y) al server Flask.
     """
     data = {
         "x": x,
         "y": y
     }
-    response = requests.post(url, json=data)
-    if(response.status_code == 204):
-        print(f"Sent: {data}")
-    elif(response.status_code == 404):
-        print(f"Error 404: {response.text}")
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 204:
+            print("Posizione aggiornata con successo")
+        else:
+            print(f"Errore: {response.status_code}")
+    except requests.ConnectionError as e: 
+        print(f"Connection error: {e}")
+    except requests.Timeout as e:
+        print(f"Request timed out: {e}")
 
 try:
     while True:
-        if ser.in_waiting > 0: 
+        if ser.in_waiting > 0:
             data = ser.readline().decode('utf-8').strip()
             print(f"Received: {data}")
-            #Qua metterò la parte che fa la richiesta al server 
-            if("," in data):
-                x, y = data.split(",")
-                send_position(float(x),float(y)) #invio al server la posizione x,y che ho letto dal joystick via seriale
-        time.sleep(0.1)
+            #legge da seriale e stampa a video 
+            try:
+                position = json.loads(data)
+                if "X" in position and "Y" in position:
+                    x = position["X"]
+                    y = position["Y"]
+                    send_position(x, y)  # Invia al server la posizione x,y che ho letto dal joystick via seriale
+                else:
+                    print(f"Invalid data format: {data}")
+            except json.JSONDecodeError:
+                print(f"Error decoding JSON: {data}")
 except KeyboardInterrupt:
     print("\nExiting...")
 finally:
